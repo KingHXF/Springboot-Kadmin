56b34a31c2e271b735f3d17e510a990e42441447
何雪峰 <1591374884@qq.com>
2020-05-12 17:48:55 +0800

    修改包名，增加项目generator

--- boot-generator/src/main/java/com/example/generator/BootGeneratorApplication.java
+++ boot-generator/src/main/java/com/example/generator/BootGeneratorApplication.java
@@ -1,4 +1,4 @@
-package com.example.bootgenerator;
+package com.example.generator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
