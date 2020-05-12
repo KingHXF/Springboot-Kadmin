586882ca0edc71d25367bec64b15ae00a82da3ff
何雪峰 <1591374884@qq.com>
2020-05-12 17:27:59 +0800

    修改包名，第一次创建的时候没有定义好

--- boot-logging/src/main/java/com/example/logging/BootLoggingApplication.java
+++ boot-logging/src/main/java/com/example/logging/BootLoggingApplication.java
@@ -1,4 +1,4 @@
-package com.example.bootlogging;
+package com.example.logging;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- boot-logging/src/test/java/com/example/logging/BootLoggingApplicationTests.java
+++ boot-logging/src/test/java/com/example/logging/BootLoggingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.bootlogging;
+package com.example.logging;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
